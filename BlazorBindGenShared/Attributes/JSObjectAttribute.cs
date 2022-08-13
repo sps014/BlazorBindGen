6633138d07abb248387693a9b41e3af5cbbdf767
Shivendra Pratap Singh <shivendrapsingh014@gmail.com>
2022-08-13 17:54:11 +0530

    Update JSObjectAttribute.cs

--- BlazorBindGenShared/Attributes/JSObjectAttribute.cs
+++ BlazorBindGenShared/Attributes/JSObjectAttribute.cs
@@ -7,7 +7,7 @@ namespace BlazorBindGen.Attributes;
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = false, Inherited = false)]
 public class JSObjectAttribute:Attribute
 {
-    public JSObjectAttribute(string Name = null)
+    public JSObjectAttribute(string Name = null,string importUrl=null)
     {
     }
 }
