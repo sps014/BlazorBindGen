81517d68feba74fe8f1e072c7e0140c1981cd767
Shivendra Pratap Singh <shivendrapsingh014@gmail.com>
2025-08-20 11:54:06 +0530

    feat: initial rewrite

--- BlazorBindGen/BindGen.cs
+++ BlazorBindGen/BindGen.cs
@@ -1,39 +1,29 @@
-﻿using BlazorBindGen.Bindings;
-using Microsoft.JSInterop;
+﻿using Microsoft.JSInterop;
 using System.Diagnostics.CodeAnalysis;
 using BlazorBindGen.Utils;
+using System.Reflection;
 
 namespace BlazorBindGen
 {
     /// <summary>
     /// Class Responsible for generating the bindings for a given type.
     /// </summary>
-    public static class BindGen
+    public static class BindGen 
     {
         
         #nullable disable
         /// <summary>
         /// Used in only Web Assembly Context for faster Interops
         /// </summary>
-        public static IJSInProcessObjectReference WasmModule { get; private set; }
+        internal static IJSInProcessObjectReference WasmModule { get; private set; }
         
         /// <summary>
         /// Used in Server Context for synchronized Interops
         /// </summary>
-        public static IJSObjectReference ServerModule { get; private set; }
+        internal static IJSObjectReference ServerModule { get; private set; }
 
-        public static IJSObjectReference CommonModule => IsWasm ? WasmModule : ServerModule;
+        internal static IJSObjectReference CommonModule => IsWasm ? WasmModule : ServerModule;
         
-        /// <summary>
-        /// get Current JS Window.
-        /// eg. window.document.getElementById("id") here Window is the current window.
-        /// </summary>
-        public static JWindow Window { get; private set; }
-        
-        /// <summary>
-        /// Dotnet Object to handle callbacks from JS
-        /// </summary>
-        private static DotNetObjectReference<JCallBackHandler> _dotNet;
         /// <summary>
         /// Used to load JS isolated modules in WASM
         /// </summary>
@@ -67,114 +57,55 @@ namespace BlazorBindGen
 
             }
             //Create DotNet Object to handle callbacks from JS and Also create a window object
-            _dotNet = DotNetObjectReference.Create(new JCallBackHandler());
-            Window = await JWindow.CreateJWindowObject();
 
-            await InitDotNet();
         }
-        /// <summary>
-        /// Pass .NET object to JS so it can call C# for Callbacks notification
-        /// </summary>
-        private static async ValueTask InitDotNet()
-        {
-            if (IsWasm)
-                WasmModule.InvokeVoid("initDotnet", _dotNet);
-            else
-                await ServerModule.InvokeVoidAsync("initDotnet", _dotNet);
-        }
-#nullable restore
-        
-        /// <summary>
-        /// Dispose the BindGen Runtimes
-        /// </summary>
-        public static async ValueTask DisposeAsync()
-        {
-            if (IsWasm)
-                await WasmModule.DisposeAsync();
-            else
-                await ServerModule.DisposeAsync();
-        }
-        
-        /// <summary>
-        /// A Fast Method to get JS pointer to byte Array in WASM platform
-        /// </summary>
-        /// <param name="array">Array from C# to sent to JS</param>
-        /// <returns>JS pointer</returns>
-        /// <exception cref="PlatformNotSupportedException"></exception>
-        public static JObjPtr SetArrayToRef(byte[] array)
-        {
-            if (!IsWasm)
-                PlatformUnsupportedException.Throw();
 
-            var obj = new JObjPtr();
-            WasmModule.InvokeVoid("SetArrayToRef", array, obj.Hash);
-            return obj;
-        }
-        /// <summary>
-        /// Fastest way to Get Byte Array from JS pointer in WASM platform
-        /// </summary>
-        /// <param name="jsUint8ArrayRef"> js pointer uint8 array </param>
-        /// <returns>byte array representation</returns>
-        /// <exception cref="PlatformNotSupportedException"></exception>
-        /// <exception cref="Exception"></exception>
-        public static byte[] GetArrayFromRef(JObjPtr jsUint8ArrayRef)
+
+        private static readonly Dictionary<Delegate, DotNetObjectReference<object>> _callbackReferences = new();
+
+
+        public static void AddEventListener(this IJSInProcessObjectReference jSInProcessObjectReference,string eventName,Delegate callback)
         {
-            if (!IsWasm)
-                throw new PlatformNotSupportedException("Only intended for wasm");
-            //if has length property then it is a JS Array
-            if (!jsUint8ArrayRef.IsProp("length"))
-                throw new Exception("Invalid js array reference, make sure the pointer to  array from js should be correct.");
-            
-            //Get length of array
-            var l = FastLength(jsUint8ArrayRef);
-            var arr = new byte[l];
-            
-            //Get array from js
-            WasmModule.InvokeVoid("GetArrayRef", arr, jsUint8ArrayRef.Hash);
-            return arr;
+            var target = callback.Target;
+
+            if(target == null)
+                throw new ArgumentNullException("Please pass instance method instead of static method");
+
+            if (!callback.Method.IsPublic || callback.Method.IsGenericMethod)
+                throw new ArgumentException("Callback method should be public and non generic");
+
+            var jSInvokable = callback.Method.GetCustomAttribute<JSInvokableAttribute>();
+
+            if (jSInvokable is null)
+                throw new ArgumentNullException("[JSInvocableAttribute] Not found on callback method");
+
+            var methodName = jSInvokable.Identifier ?? callback.Method.Name.Split(".").Last();
+
+            var dotnetObjectReference = DotNetObjectReference.Create(callback.Target);
+
+            try
+            {
+                WasmModule.InvokeVoid("addEventListenerCSharp", jSInProcessObjectReference, eventName,methodName,dotnetObjectReference);
+                _callbackReferences.Add(callback, dotnetObjectReference);
+            }
+            catch(Exception)
+            {
+                dotnetObjectReference.Dispose();
+            }
         }
-        /// <summary>
-        /// Get Length of Uint8 array JS in WASM
-        /// </summary>
-        /// <param name="jsUint8ArrayRef"> pointer to Unit8Array</param>
-        /// <returns>size of array</returns>
-        private static long FastLength(JObjPtr jsUint8ArrayRef) => 
-            WasmModule.Invoke<long>("FastLength", jsUint8ArrayRef.Hash);
-        
-        /// <summary>
-        /// Import external or internal JS module, equivalent to import in JS
-        /// </summary>
-        /// <param name="moduleUrl">uri of the module</param>
-        public static ValueTask ImportAsync(string moduleUrl)
+
+        public static void RemoveEventListener(this IJSInProcessObjectReference jSInProcessObjectReference, string eventName, Delegate callback)
         {
-            //Increment Sync callback id
-            long errH = Interlocked.Increment(ref JCallBackHandler.SyncCounter);
+            WasmModule.InvokeVoid("removeEventListenerCSharp", jSInProcessObjectReference, eventName);
 
-            CommonModule.InvokeVoidAsync("ImportGen", moduleUrl, (int)errH).ConfigureAwait(false);
-            
-            //wait until both runtimes are synced
-            return LockHandler.HoldVoid(errH);
+            if (_callbackReferences.TryGetValue(callback,out DotNetObjectReference<object> dotnetRuntime))
+            {
+                dotnetRuntime.Dispose();
+                _callbackReferences.Remove(callback);
+            }
         }
-        /// <summary>
-        /// Import external or internal JS module, equivalent to import in JS but with return pointer
-        /// eg. var a =await import("./a.js");
-        /// </summary>
-        /// <param name="moduleUrl">uri of the module</param>
-        public static async ValueTask<JObjPtr> ImportRefAsync(string moduleUrl)
-        {
-            //Increment Sync callback id
 
-            JObjPtr obj = new();
-            long errH = Interlocked.Increment(ref JCallBackHandler.SyncCounter);
 
-#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-                CommonModule.InvokeVoidAsync("ImportReturn", moduleUrl, (int)errH,obj.Hash).ConfigureAwait(false);
-#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-
-                //wait until both runtimes are synced
-            await LockHandler.HoldVoid(errH);
-            return obj;
-        }
 
     }
 }
--- BlazorBindGen/wwwroot/BlazorBindGen.js
+++ BlazorBindGen/wwwroot/BlazorBindGen.js
@@ -1,138 +1,36 @@
-﻿let props = {};
-let dotnet;
+﻿const eventHandlerMap = new Map();
 
-let callbackResult = {};
-let cbCount = 0;
+export function addEventListenerCSharp(obj, eventName, funcName, dotnet) {
+    const key = "addEventListener";
+    if (!(key in obj)) {
+        console.error(`cant attach event ${eventName} due to unavailability of ${key}() `);
+        return;
+    }
 
-export function initDotnet(net) {
-    dotnet = net;
-}
-export function CreateWin(h) {
-    props[h] = window;
-}
-export let PropVal = (pname, h) => props[h][pname];
+    // unique key per subscription
+    const subscriptionKey = `${eventName}_${funcName}`;
 
+    // create handler once
+    const handler = (...args) => dotnet.invokeMethod(funcName, ...args);
 
-export function IsNullOrUndefined(h) {
-    return props[h] === undefined || props[h] === null;
-}
-export function GetAllProperties(h) {
-    const res = [];
-    for (const p in props[h])
-        res.push(p);
-    return res;
+    // store it
+    eventHandlerMap.set(subscriptionKey, handler);
+
+    obj[key](eventName, handler);
 }
 
-export function PropRef(pname, proph, h) {
-    props[proph] = props[h][pname];
-}
-export function DeletePtr(phash) {
-    delete props[phash];
-}
-export let IsProp = (pname, h) => typeof (props[h][pname]) != "function"
-    && typeof (props[h][pname]) !== undefined;
+export function removeEventListenerCSharp(obj, eventName, funcName) {
+    const key = "removeEventListener";
+    if (!(key in obj)) {
+        console.error(`cant detach event ${eventName} due to unavailability of ${key}() `);
+        return;
+    }
 
-export let IsFunc = (pname, h) => typeof (props[h][pname]) == "function";
+    const subscriptionKey = `${eventName}_${funcName}`;
+    const handler = eventHandlerMap.get(subscriptionKey);
 
-export function PropSet(pname, val, h) {
-    props[h][pname] = val;
-}
-export function PropSetRef(pname, ph, h) {
-    props[h][pname] = props[ph];
-}
-export function Func(fname,params, h) {
-    return props[h][fname](...paramexpand(params));
-}
-export function FuncRef(fname, params, ph, h) {
-    props[ph] = props[h][fname](...paramexpand(params));
-}
-export function FuncVoid(fname, params, h) {
-    props[h][fname](...paramexpand(params));
-}
-export async function FuncRefAwait(fname, params, eh, ph,h) {
-    let er = "";
-    try { props[ph] = await props[h][fname](...paramexpand(params)); }
-    catch (e) { er = e.toString(); }
-    dotnet.invokeMethodAsync("errorMessage", eh, er, null);
-}
-export async function FuncVoidAwait(fname, params, eh,h) {
-    let er = "";
-    try { await props[h][fname](...paramexpand(params)); }
-    catch (e) { er = e.toString(); }
-    dotnet.invokeMethodAsync("errorMessage", eh, er, null);
-}
-export async function FuncAwait(fname, params, eh, h){
-    let er = "",v=null;
-    try { v = await props[h][fname](...paramexpand(params)); }
-    catch (e) { er = e.toString(); }
-    dotnet.invokeMethodAsync("errorMessage", eh, er, v);
-}
-export async function ImportGen(module, eh) {
-    let er = "";
-    try { await import(module); }
-    catch (e) { er = e.toString(); }
-    dotnet.invokeMethodAsync("errorMessage", eh, er, null);
-}
-export async function ImportReturn(module, eh, h) {
-    let er = "", v = null;
-    try { props[h] = await import(module); }
-    catch (e) { er = e.toString(); }
-    dotnet.invokeMethodAsync("errorMessage", eh, er, null);
-}
-export function Construct(classname, param, eh, h) {
-    props[eh] = new props[h][classname](...paramexpand(param));
-}
-export function SetArrayToRef(array, eh) {
-    props[eh] = Blazor.platform.toUint8Array(array);
-}
-export function GetArrayRef(array, eh) {
-    Blazor.platform.toUint8Array(array).set(props[eh]);
-}
-export function FastLength(h) {
-    return props[h].byteLength;
-}
-export function SetCallback(pname, dotnet, h) {
-    props[h][pname] = callbackHandler.bind(dotnet);
-}
-
-export let AsJson=(h)=>JSON.stringify(props[h]);
-export let To = (h) => props[h];
-
-function paramexpand(param) {
-    let res = [];
-    param.forEach((pm) => {
-        let r;
-        switch (pm.type) {
-            case 1:
-                r = props[pm.value];
-                break;
-            case 2:
-                r = callbackHandler.bind(pm.value);
-                break;
-            default:
-                r = pm.value;
-                break;
-        }
-        res.push(r);
-    });
-    return res;
-}
-function callbackHandler() {
-    let arg = [];
-    for (let i = 0; i < arguments.length; i++) 
-        arg.push(arguments[i]);
-    let h = cbCount++;
-    callbackResult[h] = arg;
-    this.invokeMethodAsync("ExecuteInCSharp", h, arg.length);
-}
-export function CleanUpArgs(cbh, h) {
-    props[h] = callbackResult[cbh];
-    delete callbackResult[cbh];
-}
-export function isEqualRef(oh, h) {
-    return props[oh] === props[h];
-}
-export function logPtr( h)
-{
-    console.log(props[h]);
+    if (handler) {
+        obj[key](eventName, handler);
+        eventHandlerMap.delete(subscriptionKey);
+    }
 }
